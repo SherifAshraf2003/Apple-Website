fe0a27a4d59efed0e9ebab90d8691521ce909397
SherifAshraf2003 <sherifelamir2003@gmail.com>
2024-09-04 23:54:18 +0300

    adjusted it back

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
   return (
     <section className='w-full nav-height bg-black relative'>
     <div className='h-5/6 w-full flex-center flex-col '>
-      <p id='hero' className='hero-title'>send ur tits sama</p>
+      <p id='hero' className='hero-title'>iPhone 15 Pro</p>
       
       <div className='md:w-10/12 w-9/12 '>
         <video className='pointer-events-none mx-auto' autoPlay muted playsInline key={videoSrc}>
